ab324eb005b9f56246d0d3143997bbe3f4e6d299
Aviad Raviv-Vash <aviad.raviv@gmail.com>
2019-11-03 04:24:59 +0200

    Changed all tags to have the iTerm prefix Added "use public ip" option Moved local variables to be set within the instances loop, so they are reset on each iteration. Added some more ssh options Changed readme moved the hosts update function to be after the iterm one, for better readability of code

--- README.md
+++ README.md
@@ -1,30 +1,32 @@
 # iTerm-cloud-profile-generator
 
+The purpose of this script, is to connect to cloud providers (currently only AWS is supported) and generate iTerm profiles.
+It is a fork of "https://gist.github.com/gmartinerro/40831c8874ebb32bc17711af95e1416b", which gave me a good starting point. With that said, currently this version of the script doesn't change the hosts file, so that it can be ran without sudo.
+
 This project currently has some assumptions:
 - When running "locally", the system is MacOS
+- You have iTerm installed
 - The awscli + profiles and credentials are already setup on your system and you're using the default locations for the configuration files.
 - You're connecting to the machines connecting your own user + key.
 - Your system has python3 installed
-- The default is to use internal IPs for instances (you can change this by adding the tag "external_ip") TODO.
+- The default is to use internal IPs for instances (you can change this by adding the tag "ip_public") TODO.
 
 # How to use (local)
 - Clone the repo
 
 `git clone https://github.com/aviadra/iTerm-cloud-profile-generator`
-- Change the permissions on the iTerm dynamic profiles directory, so that you can change it without invoking the script with sudo rights (TODO)
-
-`chown $(whoami):staff /Application/iTerm/dynamic`
 - Using python3 run the script
 
 `python3 update-aws-hosts.py`
 - You should see the dynamic profiles populated in iTerm
 
 # AWS setup
-In general there really isn't anything you "need" to do on the AWS side. With that said, there are TAGs you can add to instances and/or VPCs, to toggle script behavior.
+In general there really isn't anything you "need" to do on the AWS side. However seeing that be default only internal IPs are used (original script behaver), you would have to be VPNed to the VPC in order to use the profiles. So there are TAGs you can add to instances and/or VPCs, to toggle script behavior, and the "bastion" and/or "use_ip_public" should set unless you are using a VPN.
+All the iTerm tags are prefixed with "iTerm" and they are:
 - VPC tags
-  - You can add the "bastion" tag to a VPC, and VMs within that VPC will automatically use it.
-  - Default profile to inherit colors from TODO    
-- Instance(VM) tags
-  - You can specify the "bastion" tag. Doing so overrides the VPC "default" one.
-  - You can use the "external_ip" tag to note that this instance profile, should use the public IP for the connection. TODO
-    - When using "external_ip", the bastion is not used. unless you specify "bastion_use = yes".
+  - You can add the "iTerm_bastion" tag to a VPC, and instances within that VPC will automatically use it.
+  - Default profile to inherit colors from is set with iTerm_profile TODO
+- Instance tags
+  - You can specify the "iTerm_bastion" tag. Doing so overrides the VPC "default" one.
+  - You can use the "iTerm_use_ip_public" tag to note that this instance profile, should use the public IP for the connection. TODO
+    - When using "iTerm_ip_public", the bastion is not used. unless you specify "iTerm_bastion_use = yes".
--- update-aws-hosts.py
+++ update-aws-hosts.py
@@ -21,9 +21,6 @@ def getEC2Instances(profile_to_use):
 
     groups = {}
     instances = {}
-    bastion=''
-    vpc_bastion=''
-    instance_bastion=''
 
     boto3.setup_default_session(profile_name=profile_to_use)
 
@@ -40,7 +37,7 @@ def getEC2Instances(profile_to_use):
         for VPCs in response_vpc['Vpcs']:
             # if 'Tags' in response_vpc['Vpcs']:
             for tag in VPCs['Tags']:
-                    if tag['Key'] == 'Bastion':
+                    if tag['Key'] == 'iTerm_bastion':
                             vpc_bastion = tag['Value']
                             break
         return vpc_bastion
@@ -55,19 +52,28 @@ def getEC2Instances(profile_to_use):
             ]
     )
 
-
-    
-
-    
     for reservation in response['Reservations']:
+            bastion=''
+            vpc_bastion=''
+            instance_bastion=''
+            instance_use_ip_public=''
+            instance_use_bastion=''
             for instance in reservation['Instances']:      
                     for tag in instance['Tags']:
                             if tag['Key'] == 'Name':
                                     name = tag['Value']
                                     break
                     for tag in instance['Tags']:
-                            if tag['Key'] == 'Bastion':
+                            if tag['Key'] == 'iTerm_bastion':
                                     instance_bastion = tag['Value']
+                                    break
+                    for tag in instance['Tags']:
+                            if tag['Key'] == 'iTerm_use_ip_public':
+                                    instance_use_ip_public = tag['Value']
+                                    break
+                    for tag in instance['Tags']:
+                            if tag['Key'] == 'iTerm_use_bastion':
+                                    instance_use_bastion = tag['Value']
                                     break
                     ip = instance['NetworkInterfaces'][0]['PrivateIpAddress']
         
@@ -83,14 +89,53 @@ def getEC2Instances(profile_to_use):
                     if instance_bastion:
                         bastion = instance_bastion
 
-                    instances[ip] = {'name':'aws.' + profile_to_use + '.' + name,'index':groups[name],'group':name, 'bastion': bastion, 'vpc':reservation['Instances'][0]['VpcId']}
-                    print(ip + "\t" + 'aws.' + profile_to_use + "." + name + "\t\t bastion: \"" + bastion + "\"")
+                    instances[ip] = {'name':'aws.' + profile_to_use + '.' + name,'index':groups[name],'group':name, 'bastion': bastion, 'vpc':reservation['Instances'][0]['VpcId'], 'instance_use_ip_public': instance_use_ip_public, 'instance_use_bastion': instance_use_bastion, 'ip_public': instance['PublicIpAddress']}
+                    # print(json.dumps(instances[ip], sort_keys=True, indent=4))
+                    print(ip + "\t" + 'aws.' + profile_to_use + "." + name + "\t\t associated bastion: \"" + bastion + "\"")
    
     for ip in instances:
         instance = instances[ip]
         instance['name'] = instance['name'] + str(instance['index']) if groups[instance['group']] > 1 else instance['name']
 
     return instances, groups
+
+def updateTerm(instances,groups,profile_to_use):
+    handle = open('/Users/' + username + '/Library/Application Support/iTerm2/DynamicProfiles/aws-' + profile_to_use,'wt')
+    state = False
+
+    profiles = []
+
+    for instance in instances:
+        shortName = instances[instance]['name'][4:]
+        group = instances[instance]['group']       
+        tags =["Account: " + profile_to_use,"AWS",group] if groups[group] > 1 else ["Account: " + profile_to_use,'AWS']
+        name = instances[instance]['name']
+
+        if instances[instance]['instance_use_ip_public'] == "yes":
+            ip_for_connection = instances[instance]['ip_public']
+        else:
+            ip_for_connection = instance
+        
+
+        if (instances[instance]['bastion'] and instances[instance]['instance_use_ip_public'] != "yes") or instances[instance]['instance_use_bastion'] == "yes":
+            connection_command="ssh "  + ip_for_connection + " -J " + instances[instance]['bastion'] + " -oStrictHostKeyChecking=no -oUpdateHostKeys=yes -oServerAliveInterval=30 -oAddKeysToAgent=no"
+        else:
+            connection_command="ssh "  + ip_for_connection + " -oStrictHostKeyChecking=no -oUpdateHostKeys=yes -oServerAliveInterval=30 -oAddKeysToAgent=no"
+            
+        profile = {"Name":name,
+                    "Guid":name,
+                    "Badge Text":shortName,
+                    "Tags":tags,
+                    "Dynamic Profile Parent Name": "Bastión AWS",
+                    "Custom Command" : "Yes",
+                    "Initial Text" : connection_command
+                    }
+
+        profiles.append(profile)
+
+    profiles = {"Profiles":(profiles)} 
+    handle.write(json.dumps(profiles,sort_keys=True,indent=4, separators=(',', ': ')))
+    handle.close()
 
 # Updates the /etc/hosts file with the EC2 private addresses
 # /etc/hosts must include the list of EC2 instances between two lines: the first contains '# AWS EC2' 
@@ -124,38 +169,6 @@ def updateHosts(instances,groups):
 	
     hout.write(endDelimiter + "\n")
     hout.close()
-
-def updateTerm(instances,groups,profile_to_use):
-    handle = open('/Users/' + username + '/Library/Application Support/iTerm2/DynamicProfiles/aws-' + profile_to_use,'wt')
-    state = False
-
-    profiles = []
-
-    for instance in instances:
-        shortName = instances[instance]['name'][4:]
-        group = instances[instance]['group']       
-        tags =["Account: " + profile_to_use,"AWS",group] if groups[group] > 1 else ["Account: " + profile_to_use,'AWS']
-        name = instances[instance]['name']
-
-        if instances[instance]['bastion']:
-            connection_command="ssh -J " + instances[instance]['bastion'] + " -oStrictHostKeyChecking=no -oUpdateHostKeys=yes " + instance
-        else:
-            connection_command="ssh -oStrictHostKeyChecking=no -oUpdateHostKeys=yes " + instance
-            
-        profile = {"Name":name,
-                    "Guid":name,
-                    "Badge Text":shortName,
-                    "Tags":tags,
-                    "Dynamic Profile Parent Name": "Bastión AWS",
-                    "Custom Command" : "Yes",
-                    "Initial Text" : connection_command
-                    }
-
-        profiles.append(profile)
-
-    profiles = {"Profiles":(profiles)} 
-    handle.write(json.dumps(profiles,sort_keys=True,indent=4, separators=(',', ': ')))
-    handle.close()
 
 
 username = getpass.getuser()
