bfc90b4cfb30d2aa03ef04bcfffac8123e75b6c3
Aviad Raviv-Vash <aviad@cyesec.com>
2020-06-27 12:50:47 +0300

    Startup: Added update segment

--- startup.sh
+++ startup.sh
@@ -6,7 +6,22 @@ Config_File=".iTerm-cloud-profile-generator/config.yaml"
 Personal_Config_File="~/${Config_File}"
 SRC_Static_Profiles="/home/appuser/iTerm2-static-profiles"
 DynamicProfiles_Location="Library/Application\ Support/iTerm2/DynamicProfiles/"
-SRC_Docker_Image="aviadra/cp:${CP_Version}"
+SRC_Docker_image_base="aviadra/cp"
+SRC_Docker_Image="${SRC_Docker_image_base}:${CP_Version}"
+
+Normal_docker_start{
+    echo "Starting Cloud-profiler service\n"
+    docker run \
+        --init \
+        --restart=always \
+        -d \
+        --name cloud-profiler \
+        -e CP_Service=True \
+        -v "$(eval echo ${Personal_Config_File}:/home/appuser/${Config_File} )" \
+        -v "$(eval echo ${Personal_Static_Profiles}/:${SRC_Static_Profiles} )" \
+        -v "$(eval echo ~/${DynamicProfiles_Location}:/home/appuser/${DynamicProfiles_Location} )" \
+        ${SRC_Docker_Image}
+}
 
 if [[ ! -e $(eval echo ${Personal_Static_Profiles} ) || \
       ! -e $(eval echo ${Personal_Config_File} ) || \
@@ -34,17 +49,23 @@ then
 fi
 if [[ -z "$(docker ps -q -f name=cloud-profiler)" ]]; then
     echo "Starting Cloud-profiler service\n"
-    docker run \
-        --init \
-        --restart=always \
-        -d \
-        --name cloud-profiler \
-        -e CP_Service=True \
-        -v "$(eval echo ${Personal_Config_File}:/home/appuser/${Config_File} )" \
-        -v "$(eval echo ${Personal_Static_Profiles}/:${SRC_Static_Profiles} )" \
-        -v "$(eval echo ~/${DynamicProfiles_Location}:/home/appuser/${DynamicProfiles_Location} )" \
-        ${SRC_Docker_Image}
+    Normal_docker_start
 else
     echo -e "Cloud-profiler service already running\n"
+    echo -e "Checking for updates\n"
+    on_system_digests=$(docker images ${SRC_Docker_image_base} --format "{{.Digest}}")
+    latest_version_raw=$( docker pull ${SRC_Docker_Image})
+    latest_version_digets="$( echo $latest_version_raw | grep Digest | awk '{print $2}' )"
+    if [[ ${latest_version_digets} != ${on_system_digests} ]]; then
+      echo -e "Newer version of container detected.\n"
+      echo -e "Now restarting service for changes to take affect."
+      docker stop cloud-profiler
+      docker rm cloud-profiler
+      Normal_docker_start
+    fi
+    echo -e "Issuing ad-hoc run."
+    docker exec \
+        cloud-profiler \
+        python3 update-cloud-hosts.py
 fi
 docker ps -f name=cloud-profiler
