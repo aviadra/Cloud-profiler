cfd2eaadf5db4d585165f3d856ca48286c648c97
Aviad Raviv-Vash <aviad.raviv@gmail.com>
2021-02-17 22:57:14 +0200

    v4.4.1: Version bump.

--- update-cloud-hosts.py
+++ update-cloud-hosts.py
@@ -1004,7 +1004,7 @@ def do_worker(do_script_config, do_instance_counter, do_cloud_instances_obj_list
 
 # MAIN
 if __name__ == '__main__':
-    VERSION = "v4.4.0"
+    VERSION = "v4.4.1"
     with open("marker.tmp", "w") as file:
         file.write("mark")
 
