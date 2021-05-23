4a1bb2ad116f934a2474772416c51469a737c1f2
aviad <aviad@cyesec.com>
2021-05-24 00:19:41 +0300

    Ruri'iro Kujaku: Version bump

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-[ -z ${CP_Version+x} ] && CP_Version='v5.0.3'
+[ -z ${CP_Version+x} ] && CP_Version='v5.1.0'
 Personal_Static_Profiles="${HOME}/iTerm2-static-profiles"
 SRC_Static_Profiles="/home/appuser/iTerm2-static-profiles"
 SRC_Docker_image_base="aviadra/cp"
