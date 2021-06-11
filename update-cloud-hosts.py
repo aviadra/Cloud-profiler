30981e9a73dd33ded38e60bf813af6c97bdc596d
aviad <aviad@cyesec.com>
2021-06-11 09:32:22 +0300

    v6.0.1_Chasey_Lain: version bump

--- update-cloud-hosts.py
+++ update-cloud-hosts.py
@@ -1311,7 +1311,7 @@ def checkinternetrequests(url='http://www.google.com/', timeout=3, verify=False,
 
 # MAIN
 if __name__ == '__main__':
-    VERSION = "v6.0.0_Chasey_Lain"
+    VERSION = "v6.0.1_Chasey_Lain"
     with open("marker.tmp", "w") as file:
         file.write("mark")
 
