57b6016b38d1d8cd47abb6b5fc42f64b9f3d7354
aviad <aviad@cyesec.com>
2021-06-21 00:17:30 +0300

    v6.0.3_Chasey_Amy: version bump (temp name replaced with actual)

--- update-cloud-hosts.py
+++ update-cloud-hosts.py
@@ -840,7 +840,7 @@ def update_moba(obj_list):
                 f"\nCP Update profiles {VERSION} =" \
                 f";  logout#151#14%Default%%Interactive " \
                 f"shell%__PTVIRG__[ -z ${{CP_Version+x}} ] " \
-                f"&& CP_Version__EQUAL__'v6.0.3_Chasey_Next'__PTVIRG__[ -z ${{CP_Branch+x}} ] " \
+                f"&& CP_Version__EQUAL__'v6.0.3_Chasey_Amy'__PTVIRG__[ -z ${{CP_Branch+x}} ] " \
                 f"&& CP_Branch__EQUAL__'main'__PTVIRG__" \
                 f"[ __DBLQUO__${{CP_Branch}}__DBLQUO__ __EQUAL____EQUAL__ __DBLQUO__develop__DBLQUO__ ] " \
                 f"&& CP_Version__EQUAL__'edge'__PTVIRG__" \
@@ -1329,7 +1329,7 @@ def checkinternetrequests(url='http://www.google.com/', timeout=3, verify=False,
 
 # MAIN
 if __name__ == '__main__':
-    VERSION = "v6.0.3_Chasey_Next"
+    VERSION = "v6.0.3_Chasey_Amy"
     with open("marker.tmp", "w") as file:
         file.write("mark")
 
